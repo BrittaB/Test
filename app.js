912fc2fabfed90948c44e691f036d48dbb181396
Britta <polished.athena@gmail.com>
2019-06-02 11:56:35 -0500

    updated app

--- app.js
+++ app.js
@@ -9,3 +9,20 @@ const firebaseConfig = {
     messagingSenderId: "1013064070830",
     appId: "1:1013064070830:web:499c11fabe7f0d4f"
   };
+
+  document.addEventListener("DOMContentLoaded", event => {
+
+    const app =firebase.app();
+
+  });
+
+  function gitHubLogin() {
+    cosnt provider = new firebase.auth.GithubAuthProvider();
+
+    firebase.auth().signInWithPopup(provider)
+
+      .then(result => {
+          const user = sesult.user;
+          
+      })
+  }
