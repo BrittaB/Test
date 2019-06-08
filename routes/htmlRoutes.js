dc5c937331f695248a4d6c59beedac5656fc0902
Britta <polished.athena@gmail.com>
2019-06-08 11:08:40 -0500

    updated project

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,25 +3,37 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+    db.projectExamples.findAll({}).then(function(dbprojectExamples) {
       res.render("index", {
         msg: "Welcome!",
-        examples: dbExamples
+        examples: dbprojectExamples
       });
     });
   });
-
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
+  
+  app.get('/trending', function(req, res) {
+    res.render('trending');
   });
 
+  app.get('/projects', function(req, res) {
+    res.render('projects');
+  });
+
+  app.get('/contact', function(req, res) {
+    res.render('contact');
+  });
+
+  app.get('/collaborate', function(req, res) {
+    res.render('collaborate');
+  });
+
+  
+  
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
     res.render("404");
   });
+
+
 };
+
